e1c38d6a06b6b2ccd4394541c8ed2b08da632947
Atharv-G <87017866+Atharv-G@users.noreply.github.com>
2021-07-06 15:00:37 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ball
+function setup() {
+
+  createCanvas(400,400);
+  ball = createSprite(200,200,20,20);
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+  if (keyDown(RIGHT_ARROW)){
+    ball.x += 5
+  }
+
+  if (keyDown(LEFT_ARROW)){
+    ball.x -= 5
+  }
+
+  if (keyDown(UP_ARROW)){
+    ball.y -= 5
+  }
+
+  if (keyDown(DOWN_ARROW)){
+    ball.y += 5
+  }
+
+}
